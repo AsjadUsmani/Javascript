5be4c6062f3875b4a1d9b4e13328526d8e8a106d
AsjadUsmani <156511939+AsjadUsmani@users.noreply.github.com>
2025-04-30 19:15:20 +0000

    02. Variables done

---
+++ 02_variables/variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553;
+let accountEmail = "asjadusmani@gmail.com";
+
+var accountPassword = 12345;
+
+// accountId = 2 not allowed
+console.table([accountEmail, accountId, accountPassword]);
+/* 
+prefer not to use Var 
+because of issue in block scope and functional scope
+*/
