4231c5f6699c2005940ec205b5e3ff78acfd3929
AsjadUsmani <156511939+AsjadUsmani@users.noreply.github.com>
2025-05-28 12:27:01 +0000

    conversion update and conversion 2 added

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,31 +1,68 @@
-let age = "33abc";
+let score = "hitesh"
 
-// console.log(typeof age);
+//console.log(typeof score);
+//console.log(typeof(score));
 
-let ageInNumber = Number(age);
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
-// console.log(typeof ageInNumber);
-// console.log(ageInNumber);
 
-/*Numbers me convert karte waqt :-
-null - agar null hoti value to uska type object
-or uska number conversion baad value hai wo
-0 rehti
-undefined - iski value NaN rehti hai
-boolian - iski value true hai to 1 or false hai to 0
-rahegi
-*/
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
 
-let login = 1;
-let emptyString = "";
-let name = "Asjad";
+let isLoggedIn = "hitesh"
 
-console.log(Boolean(login));
-console.log(Boolean(emptyString));
-console.log(Boolean(name));
-/* Boolean:-
-1 => true
-0 => false
-"" => false
-"Asjad" => true
-*/
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
