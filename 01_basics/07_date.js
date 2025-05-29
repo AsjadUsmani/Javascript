f15fe3d5cce121237c31ef0d4bc20067ab087347
AsjadUsmani <156511939+AsjadUsmani@users.noreply.github.com>
2025-05-29 21:55:36 +0530

    Minor Changes

--- 01_basics/07_date.js
+++ 01_basics/07_date.js
@@ -2,7 +2,7 @@ const myDate = new Date(2026, 4, 12);
 
 console.log(myDate);
 console.log(myDate.toDateString());
-console.log(myDate.toISOString);
+console.log(myDate.toISOString());
 console.log(myDate.toLocaleDateString());
 
 myDate.toLocaleString('default', {
