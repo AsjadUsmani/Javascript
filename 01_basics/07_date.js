fbbf1c151c86e1c98cf9279faa77a5a999f27958
AsjadUsmani <156511939+AsjadUsmani@users.noreply.github.com>
2025-05-29 16:23:37 +0000

    Dates in javascript

---
+++ 01_basics/07_date.js
@@ -0,0 +1,10 @@
+const myDate = new Date(2026, 4, 12);
+
+console.log(myDate);
+console.log(myDate.toDateString());
+console.log(myDate.toISOString);
+console.log(myDate.toLocaleDateString());
+
+myDate.toLocaleString('default', {
+    day: "numeric"
+});
